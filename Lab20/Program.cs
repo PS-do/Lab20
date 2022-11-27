875966915570d17caa9e4b71ff8f2ad2599ea70b
Сергей Плюхин <psg.pgs@mail.ru>
2022-11-27 23:34:50 +0300

    Добавьте файлы проекта.

---
+++ Lab20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab20
+{
+    internal class Program
+    {
+        delegate int MyDelegate (int a, int b);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = PowerAB;
+            Console.WriteLine(myDelegate(2, 10));
+
+            myDelegate=PowerBA;
+            Console.WriteLine(myDelegate(2, 10));
+
+            Console.ReadKey();
+        }
+
+        static int PowerAB(int a, int b) => (int)Math.Pow(a, b);
+        static int PowerBA(int a, int b) => (int)Math.Pow(b, a);
+        //{
+        //    //int p = 1;
+        //    //for (int i = 0; i < b; i++)
+        //    //{
+        //    //    p *= a;
+        //    //}
+        //    //return p;
+        //    //return (int)Math.Pow(a, b);
+
+        //    return (int)(Math.Round( Math.Pow(a, b)));
+        //}
+    }
+}
